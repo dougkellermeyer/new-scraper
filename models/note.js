c58d0d265a488d39185fa9b0a3709b9a966f8314
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-06-09 00:45:17 -0400

    updated file structure for handlebars

--- models/note.js
+++ models/note.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 
 //create a new NoteSchema object
 var NoteSchema = new Schema({
-  title: String,
-  body: String
+    title: String,
+    body: String
 });
 
 var Note = mongoose.model("Note", NoteSchema);
